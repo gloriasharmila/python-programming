4de8f9f94fb7dd5ad42b7897e55a7b87e4d2282a
gloriasharmila <37238391+gloriasharmila@users.noreply.github.com>
2019-02-05 19:10:23 +0530

    Create Alphabet or not.py

---
+++ Alphabet or not.py
@@ -0,0 +1,7 @@
+list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+x=input()
+print(x)
+if(x in list):
+ print("alphabet");
+else:
+ print("not alphabet");
