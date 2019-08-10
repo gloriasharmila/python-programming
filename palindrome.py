7010488b699c4d41cab582409ba439f9245dd013
gloriasharmila <37238391+gloriasharmila@users.noreply.github.com>
2019-08-10 10:39:40 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -2,12 +2,12 @@ n=int(input())
 temp=n
 rev=0
 while(n>0):
-rem=n%10
-rev=rev*10+rem
-n=n//10
+    rem=n%10
+    rev=rev*10+rem
+    n=n//10
 if(temp==rev):
-print("yes")
+   print("yes")
 else:
-print("no")
+   print("no")
 
 
